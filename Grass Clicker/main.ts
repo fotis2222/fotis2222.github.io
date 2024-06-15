4f9e0042f2e90605db07b783b176910aaa34b449
fotis2222 <nitsas2012@gmail.com>
2024-06-15 21:45:13 +0300

    Added Grass Clicker

---
+++ Grass Clicker/main.ts
@@ -0,0 +1,14 @@
+const grassDisplay = document.getElementById("grassDisplay");
+
+let game = {
+  grass: 0,
+};
+
+function cut() {
+  game.grass += 1;
+  if (grassDisplay) grassDisplay.innerHTML = `${game.grass} Grass`;
+}
+
+setInterval(function () {
+  cut();
+}, 1000);
